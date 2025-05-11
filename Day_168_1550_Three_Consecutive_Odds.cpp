f06dfcda029200a5a36fe4fce817cee85459cd48
khushi <khushisangwan441@gmail.com>
2025-05-11 12:56:38 +0530

    Added function to check for three consecutive odd numbers in an array

---
+++ Day_168_1550_Three_Consecutive_Odds.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int count = 0;
+    for (const int a : arr) {
+      count = a % 2 == 0 ? 0 : count + 1;
+      if (count == 3)
+        return true;
+    }
+    return false;
+
+    }
+};
