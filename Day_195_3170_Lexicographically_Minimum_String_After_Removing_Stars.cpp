a6ee67e23efff9fe895ed714a7060f7f8a4fbba4
khushi <khushisangwan441@gmail.com>
2025-06-07 17:00:40 +0530

    Refactor code for clarity and consistency in the C++ solution for LeetCode 3170 – Lexicographically Minimum String After Removing Stars

--- Day_195_3170_Lexicographically_Minimum_String_After_Removing_Stars.cpp
+++ Day_195_3170_Lexicographically_Minimum_String_After_Removing_Stars.cpp
@@ -8,21 +8,26 @@ using namespace std;
 class Solution {
 public:
     string clearStars(string s) {
-         string ans = s;
-    vector<vector<int>> buckets(26);
+        string ans = s; // Copy of the input string to modify
+        vector<vector<int>> buckets(26); // Buckets to store indices of each character ('a' to 'z')
 
-    for (int i = 0; i < s.length(); ++i)
-      if (s[i] == '*') {
-        ans[i] = ' ';
-        int j = 0;
-        while (buckets[j].empty())
-          ++j;
-        ans[buckets[j].back()] = ' ', buckets[j].pop_back();
-      } else {
-        buckets[s[i] - 'a'].push_back(i);
-      }
+        // Iterate through the string
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == '*') {
+                ans[i] = ' '; // Mark the star for removal
+                int j = 0;
+                // Find the lexicographically smallest character to remove
+                while (buckets[j].empty())
+                    ++j;
+                ans[buckets[j].back()] = ' '; // Mark the character for removal
+                buckets[j].pop_back(); // Remove the index from the bucket
+            } else {
+                // Store the index of the character in its corresponding bucket
+                buckets[s[i] - 'a'].push_back(i);
+            }
+        }
 
-    std::erase(ans, ' ');
-    return ans;
+        std::erase(ans, ' '); // Remove all marked characters (spaces)
+        return ans; // Return the resulting string
     }
 };
