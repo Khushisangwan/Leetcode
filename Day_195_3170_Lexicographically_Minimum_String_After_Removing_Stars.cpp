422f6ea321c9af3a0bfa0202d1e760ac060c35b0
khushi <khushisangwan441@gmail.com>
2025-06-07 16:59:52 +0530

    Add initial comments and structure for C++ solution to LeetCode 3170 – Lexicographically Minimum String After Removing Stars

--- Day_195_3170_Lexicographically_Minimum_String_After_Removing_Stars.cpp
+++ Day_195_3170_Lexicographically_Minimum_String_After_Removing_Stars.cpp
@@ -1,3 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+// 3170. Lexicographically Minimum String After Removing Stars
+// This code defines a solution to remove stars from a string and return the lexicographically smallest string possible.
+// The solution uses a vector of buckets to store indices of characters and processes stars accordingly.
+// commit: add Solution class for lexicographically minimum string after removing stars
+
 class Solution {
 public:
     string clearStars(string s) {
