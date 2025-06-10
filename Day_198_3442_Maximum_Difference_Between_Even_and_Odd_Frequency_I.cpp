fc3974c3e5bafa3f33af44f40fc9651f355cda64
khushi <khushisangwan441@gmail.com>
2025-06-10 23:42:21 +0530

    Add header comments to clarify the C++ solution for LeetCode 344 – Maximum Difference Between Even and Odd Frequency I

--- Day_198_3442_Maximum_Difference_Between_Even_and_Odd_Frequency_I.cpp
+++ Day_198_3442_Maximum_Difference_Between_Even_and_Odd_Frequency_I.cpp
@@ -1,3 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+// 3442. Maximum Difference Between Even and Odd Frequency I
+// This code defines a solution to find the maximum difference between the frequency of characters with odd and even counts in a string.
+
 class Solution {
 public:
     int maxDifference(string s) {
