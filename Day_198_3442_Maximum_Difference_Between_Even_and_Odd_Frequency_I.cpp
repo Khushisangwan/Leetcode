7a77d3110c3a01a10f7fca02eaeb7d7c380c1c90
khushi <khushisangwan441@gmail.com>
2025-06-10 23:43:03 +0530

    Refactor comments for clarity in C++ solution for LeetCode 344 – Maximum Difference Between Even and Odd Frequency I

--- Day_198_3442_Maximum_Difference_Between_Even_and_Odd_Frequency_I.cpp
+++ Day_198_3442_Maximum_Difference_Between_Even_and_Odd_Frequency_I.cpp
@@ -5,14 +5,18 @@ using namespace std;
 
 class Solution {
 public:
+    // Function to find the maximum difference between the frequency of characters
+    // with odd and even counts in a string
     int maxDifference(string s) {
-        vector<int> mpp(26);
-        int maxi = 0, mini = s.size();
-        for (char c : s) mpp[c - 'a']++;
+        vector<int> mpp(26); // Frequency map for each lowercase letter
+        int maxi = 0, mini = s.size(); // Initialize max odd freq and min even freq
+        for (char c : s) mpp[c - 'a']++; // Count frequency of each character
         for (int i = 0 ; i < 26; i++) {
-            if (mpp[i] % 2 != 0) maxi = max(maxi, mpp[i]);
-            if (mpp[i] % 2 == 0 && mpp[i] > 0) mini = min(mini, mpp[i]);
+            if (mpp[i] % 2 != 0) // If frequency is odd
+                maxi = max(maxi, mpp[i]); // Update maximum odd frequency
+            if (mpp[i] % 2 == 0 && mpp[i] > 0) // If frequency is even and > 0
+                mini = min(mini, mpp[i]); // Update minimum even frequency
         } 
-        return maxi - mini;
+        return maxi - mini; // Return the difference
     }
 };
