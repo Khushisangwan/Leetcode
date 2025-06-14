df8b889cde3b18264d6528064e3a31aa76373201
khushi <khushisangwan441@gmail.com>
2025-06-14 23:01:36 +0530

    Enhance clarity in C++ solution for LeetCode 2566 by adding comments to key functions

--- Day_202_2566_Maximum_Difference_by_Remapping_a_Digit.cpp
+++ Day_202_2566_Maximum_Difference_by_Remapping_a_Digit.cpp
@@ -6,32 +6,39 @@ using namespace std;
 
 class Solution {
 public:
+    // Returns the maximum difference by remapping a digit in num
     int minMaxDifference(int num) {
         const string s = to_string(num);
-    const char to9 = s[firstNotNineIndex(s)];
-    const char to0 = s[0];
-    return getMax(s, to9) - getMin(s, to0);
-  }
+        // Find the first digit that is not '9' to maximize the number
+        const char to9 = s[firstNotNineIndex(s)];
+        // The first digit is used to minimize the number
+        const char to0 = s[0];
+        // Return the difference between the max and min possible numbers
+        return getMax(s, to9) - getMin(s, to0);
+    }
 
- private:
-  int firstNotNineIndex(const string& s) {
-    for (int i = 0; i < s.length(); ++i)
-      if (s[i] != '9')
-        return i;
-    return 0;
-  }
+private:
+    // Returns the index of the first digit that is not '9'
+    int firstNotNineIndex(const string& s) {
+        for (int i = 0; i < s.length(); ++i)
+            if (s[i] != '9')
+                return i;
+        return 0; // If all digits are '9', return 0
+    }
 
-  int getMax(string s, char to9) {
-    for (char& c : s)
-      if (c == to9)
-        c = '9';
-    return stoi(s);
-  }
+    // Replaces all occurrences of 'to9' with '9' to get the maximum number
+    int getMax(string s, char to9) {
+        for (char& c : s)
+            if (c == to9)
+                c = '9';
+        return stoi(s);
+    }
 
-  int getMin(string s, char to0) {
-    for (char& c : s)
-      if (c == to0)
-        c = '0';
-    return stoi(s);
+    // Replaces all occurrences of 'to0' with '0' to get the minimum number
+    int getMin(string s, char to0) {
+        for (char& c : s)
+            if (c == to0)
+                c = '0';
+        return stoi(s);
     }
 };
