70eb51e1105b816eeca881fd71c5c1d2b7f11dfb
khushi <khushisangwan441@gmail.com>
2025-06-15 20:57:40 +0530

    Enhance clarity in C++ solution for LeetCode 1432 by adding comments to key sections

--- Day_203_1432_Max_Difference_You_Can_Get_From_Changing_an_Integer.cpp
+++ Day_203_1432_Max_Difference_You_Can_Get_From_Changing_an_Integer.cpp
@@ -7,18 +7,30 @@ using namespace std;
 class Solution {
 public:
     int maxDiff(int num) {
-         const string s = to_string(num);
-    int firstNot9 = s.find_first_not_of('9');
-    int firstNot01 = s.find_first_not_of("01");
-    if (firstNot9 == string::npos)
-      firstNot9 = 0;
-    if (firstNot01 == string::npos)
-      firstNot01 = 0;
+        // Convert the integer to a string for easy digit manipulation
+        const string s = to_string(num);
+        
+        // Find the first digit that is not '9' for maximizing the number
+        int firstNot9 = s.find_first_not_of('9');
+        // Find the first digit that is not '0' or '1' for minimizing the number
+        int firstNot01 = s.find_first_not_of("01");
+        
+        // If all digits are '9', set index to 0 (no change needed)
+        if (firstNot9 == string::npos)
+            firstNot9 = 0;
+        // If all digits are '0' or '1', set index to 0 (no change needed)
+        if (firstNot01 == string::npos)
+            firstNot01 = 0;
 
-    string a = s;
-    string b = s;
-    replace(a.begin(), a.end(), s[firstNot9], '9');
-    replace(b.begin(), b.end(), s[firstNot01], firstNot01 == 0 ? '1' : '0');
-    return stoi(a) - stoi(b);
+        // Create copies of the string to modify
+        string a = s;
+        string b = s;
+        // Replace all occurrences of the first non-'9' digit with '9' to maximize
+        replace(a.begin(), a.end(), s[firstNot9], '9');
+        // Replace all occurrences of the first non-'0'/'1' digit with '1' (if first digit) or '0' (otherwise) to minimize
+        replace(b.begin(), b.end(), s[firstNot01], firstNot01 == 0 ? '1' : '0');
+        
+        // Return the difference between the maximized and minimized numbers
+        return stoi(a) - stoi(b);
     }
 };
