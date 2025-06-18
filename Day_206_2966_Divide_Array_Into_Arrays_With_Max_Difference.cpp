b1788eb5768059161a5094d532fff46260edaeb9
khushi <khushisangwan441@gmail.com>
2025-06-18 11:54:26 +0530

    Enhance clarity in C++ solution for LeetCode 2966 by adding comments to key sections of the divideArray function

--- Day_206_2966_Divide_Array_Into_Arrays_With_Max_Difference.cpp
+++ Day_206_2966_Divide_Array_Into_Arrays_With_Max_Difference.cpp
@@ -9,12 +9,17 @@ class Solution {
 public:
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
         vector<vector<int>> ans;
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n-2;i+=3){
-            if(nums[i+2]-nums[i]>k) return {};
-            ans.push_back({nums[i],nums[i+1],nums[i+2]});
+        int n = nums.size();
+        // Sort the array to group close elements together
+        sort(nums.begin(), nums.end());
+        // Iterate through the array in steps of 3
+        for (int i = 0; i < n - 2; i += 3) {
+            // Check if the difference between the largest and smallest in the group is within k
+            if (nums[i + 2] - nums[i] > k) return {};
+            // Add the group of 3 to the answer
+            ans.push_back({nums[i], nums[i + 1], nums[i + 2]});
         }
+        // Return the list of groups
         return ans;
     }
 };
