b05cd2c28b0807035c639541863f6fc9abcf7f67
khushi <khushisangwan441@gmail.com>
2025-06-23 19:50:29 +0530

    Add header comments and documentation for C++ solution of LeetCode 2081 – Sum of k-Mirror Numbers

--- Day_211_2081_Sum_of_k_Mirror_Numbers.cpp
+++ Day_211_2081_Sum_of_k_Mirror_Numbers.cpp
@@ -1,3 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+// LeetCode Problem 2081: Sum of k-Mirror Numbers
+// Link: https://leetcode.com/problems/sum-of-k-mirror-numbers/
+// Time Complexity: O(n * log(n)), where n is the number of k-mirror numbers to find.
+// Space Complexity: O(log(n)), for storing the digits of the number in a vector.
 class Solution {
 public:
     long long createPalindrome(long long num, bool odd) {
