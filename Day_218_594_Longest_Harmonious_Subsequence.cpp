3f33215e9305df88ff817add195340d4d6767b6d
khushi <khushisangwan441@gmail.com>
2025-06-30 23:25:57 +0530

    Enhance comments for clarity in C++ solution for LeetCode 594 – Longest Harmonious Subsequence

--- Day_218_594_Longest_Harmonious_Subsequence.cpp
+++ Day_218_594_Longest_Harmonious_Subsequence.cpp
@@ -5,17 +5,22 @@ using namespace std;
 class Solution {
 public:
     int findLHS(vector<int>& nums) {
+        // Sort the array to bring equal and consecutive numbers together
         sort(nums.begin(), nums.end());
         int j = 0, maxLength = 0;
 
+        // Use two pointers: i (end of window), j (start of window)
         for (int i = 0; i < nums.size(); ++i) {
+            // Move j forward if the difference is more than 1
             while (nums[i] - nums[j] > 1) {
                 ++j;
             }
+            // If the difference is exactly 1, update maxLength
             if (nums[i] - nums[j] == 1) {
                 maxLength = max(maxLength, i - j + 1);
             }
         }
+        // Return the length of the longest harmonious subsequence
         return maxLength;
     }
 };
