f2309e1145a343576f266e58254bd8d0bf9ae43d
khushi <khushisangwan441@gmail.com>
2025-07-08 22:53:07 +0530

    Enhance comments for clarity in C++ solution for LeetCode 1751 – Maximum Number of Events That Can Be Attended II

--- Day_226_1751_Maximum_Number_of_Events_That_Can_Be_Attended_II.cpp
+++ Day_226_1751_Maximum_Number_of_Events_That_Can_Be_Attended_II.cpp
@@ -5,18 +5,23 @@ using namespace std;
 // Steps:
 class Solution {
 public:
+    // Main function to find the maximum value by attending at most k non-overlapping events
     int maxValue(vector<vector<int>>& events, int k) {
+        // Sort events by their end time
         sort(events.begin(), events.end(), [](const vector<int>& a, const vector<int>& b) {
             return a[1] < b[1];
         });
 
         int n = events.size();
+        // dp[i][j]: max value by considering first i events and attending at most j events
         vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
 
         for (int i = 1; i <= n; ++i) {
+            // Find the last event that doesn't overlap with the current event
             int prev = binarySearch(events, events[i - 1][0]);
 
             for (int j = 1; j <= k; ++j) {
+                // Either skip the current event or attend it and add its value
                 dp[i][j] = max(dp[i - 1][j], dp[prev + 1][j - 1] + events[i - 1][2]);
             }
         }
@@ -25,6 +30,7 @@ public:
     }
 
 private:
+    // Binary search to find the last event that ends before currentStart
     int binarySearch(vector<vector<int>>& events, int currentStart) {
         int left = 0, right = events.size() - 1;
         int result = -1;
