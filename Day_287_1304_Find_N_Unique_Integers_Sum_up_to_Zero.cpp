387b97bd1a71fe997b0817e11b6245084d1b43ac
khushi <khushisangwan441@gmail.com>
2025-09-07 21:56:01 +0530

    feat: enhance comments for clarity in sumZero function

--- Day_287_1304_Find_N_Unique_Integers_Sum_up_to_Zero.cpp
+++ Day_287_1304_Find_N_Unique_Integers_Sum_up_to_Zero.cpp
@@ -4,10 +4,12 @@ class Solution {
 public:
     vector<int> sumZero(int n) {
         vector<int> ans;
+        // Add pairs of positive and negative numbers that cancel out
         for (int i = 1; i <= n / 2; ++i) {
             ans.push_back(i);
             ans.push_back(-i);
         }
+        // If n is odd, add 0 to complete the array
         if (n % 2 == 1) {
             ans.push_back(0);
         }
