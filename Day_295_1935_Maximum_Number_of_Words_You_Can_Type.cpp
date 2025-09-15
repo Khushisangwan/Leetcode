e444ab95779a4730698cc63c6f21a75fe6d319fc
khushi <khushisangwan441@gmail.com>
2025-09-15 21:06:44 +0530

    feat: improve code readability with enhanced comments and formatting in canBeTypedWords function

--- Day_295_1935_Maximum_Number_of_Words_You_Can_Type.cpp
+++ Day_295_1935_Maximum_Number_of_Words_You_Can_Type.cpp
@@ -3,18 +3,22 @@ using namespace std;
 class Solution {
 public:
     int canBeTypedWords(string& text, string& brokenLetters) {
-        unsigned mask=0;
+        // Create a bitmask to represent broken letters
+        unsigned mask = 0;
         for(char c: brokenLetters)
-            mask|=(1<<(c-'a'));
-        int cnt=0;
-        bool word=1;
-        text.push_back(' ');
+            mask |= (1 << (c - 'a')); // Set bit for each broken letter
+        
+        int cnt = 0;        // Count of typeable words
+        bool word = 1;      // Flag to track if current word is typeable
+        text.push_back(' '); // Add space at end to process last word
+        
         for(char c: text){
-            if (c==' '){
-                cnt+=word;
-                word=1;
+            if (c == ' '){
+                cnt += word;    // Add to count if word is typeable
+                word = 1;       // Reset flag for next word
             }
-            else if (mask &(1<<(c-'a'))) word=0;  
+            else if (mask & (1 << (c - 'a'))) 
+                word = 0;       // Mark word as untypeable if broken letter found
         }
         return cnt;
     }
