d016c39a99dbf5ca52f12016443d69648253f70d
khushi <khushisangwan441@gmail.com>
2025-09-25 23:03:00 +0530

    refactor: enhance comments for clarity in minimumTotal function

--- Day_305_120_Triangle.cpp
+++ Day_305_120_Triangle.cpp
@@ -1,13 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+// Solution class to solve the Triangle minimum path sum problem
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
+        // Start from the second last row and move upwards
         for (int i = triangle.size() - 2; i >= 0; i--)
+            // For each element in the current row
             for (int j = 0; j < triangle[i].size(); j++)
+                // Add the minimum of the two adjacent numbers from the row below
                 triangle[i][j] +=
                     min(triangle[i + 1][j], triangle[i + 1][j + 1]);
 
+        // The top element contains the minimum path sum
         return triangle[0][0];
     }
 };
