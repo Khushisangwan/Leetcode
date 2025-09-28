e723229bf6d9897c9072794d2c1614da4bd3b64d
khushi <khushisangwan441@gmail.com>
2025-09-28 23:20:50 +0530

    refactor: improve comments for clarity in largestPerimeter function

--- Day_308_976_Largest_Perimeter_Triangle.cpp
+++ Day_308_976_Largest_Perimeter_Triangle.cpp
@@ -3,10 +3,19 @@ using namespace std;
 class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
+        // Sort array in descending order to check largest possible triangles first
         sort(nums.begin(), nums.end(), greater<int>());
-        for (int i = 0; i < nums.size() - 2; i++)
-            if (nums[i + 1] + nums[i + 2] > nums[i])
+        
+        // Check each triplet starting from the largest values
+        for (int i = 0; i < nums.size() - 2; i++) {
+            // Triangle inequality: sum of two smaller sides must be greater than the largest side
+            if (nums[i + 1] + nums[i + 2] > nums[i]) {
+                // Return the perimeter of the first valid triangle found
                 return nums[i] + nums[i + 1] + nums[i + 2];
+            }
+        }
+        
+        // No valid triangle found
         return 0;
     }
 };
