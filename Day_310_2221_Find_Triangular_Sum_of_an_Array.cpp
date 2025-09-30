beec7ec096cbfa42c83ac772ea83ed4595f8deec
khushi <khushisangwan441@gmail.com>
2025-09-30 23:52:19 +0530

    refactor: enhance code readability and comments in triangularSum function

--- Day_310_2221_Find_Triangular_Sum_of_an_Array.cpp
+++ Day_310_2221_Find_Triangular_Sum_of_an_Array.cpp
@@ -3,12 +3,20 @@ using namespace std;
 class Solution {
 public:
     int triangularSum(vector<int>& nums) {
-        int n=nums.size();
-       for (int i=n-1; i>=1; i--){
-           for(int j=0; j<i; j++){
-               nums[j]=(nums[j]+nums[j+1])%10;
-           }
-       }
-       return nums[0]; 
+        int n = nums.size();
+        
+        // Iterate from bottom to top of the triangle
+        // Each iteration reduces the array size by 1
+        for (int i = n - 1; i >= 1; i--) {
+            // For each level, calculate sum of adjacent pairs
+            for (int j = 0; j < i; j++) {
+                // Add adjacent elements and take modulo 10
+                // Store result in the left position
+                nums[j] = (nums[j] + nums[j + 1]) % 10;
+            }
+        }
+        
+        // Return the final single element at the top of triangle
+        return nums[0]; 
     }
 };
