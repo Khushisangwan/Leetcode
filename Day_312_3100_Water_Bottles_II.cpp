589012c66278df572e1b9b8f61fcee21b4ef1ad5
khushi <khushisangwan441@gmail.com>
2025-10-02 23:50:52 +0530

    refactor: enhance documentation for maxBottlesDrunk function

--- Day_312_3100_Water_Bottles_II.cpp
+++ Day_312_3100_Water_Bottles_II.cpp
@@ -1,5 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+/**
+ * @brief Calculates the maximum number of bottles that can be drunk given an initial number of bottles
+ *        and an exchange mechanism where empty bottles can be traded for new full bottles.
+ * 
+ * This function simulates a bottle exchange system where:
+ * - You start with 'numBottles' full bottles
+ * - You can exchange 'x' empty bottles for 1 new full bottle
+ * - After each exchange, the required number of empty bottles increases by 1
+ * - The process continues until you don't have enough empty bottles for another exchange
+ * 
+ * @param numBottles The initial number of full bottles available to drink
+ * @param x The initial number of empty bottles required to exchange for 1 new full bottle
+ * 
+ * @return The maximum total number of bottles that can be drunk
+ * 
+ * Algorithm:
+ * 1. Start with ans = numBottles (drink all initial bottles)
+ * 2. While we have enough empty bottles (numBottles >= x):
+ *    - Exchange x empty bottles for 1 new bottle (numBottles -= x - 1)
+ *    - Increment the exchange rate (x++)
+ *    - Increment the total bottles drunk (ans++)
+ * 3. Return the total count
+ * 
+ * Time Complexity: O(sqrt(numBottles)) - the exchange rate increases linearly
+ * Space Complexity: O(1) - only using constant extra space
+ */
 class Solution {
 public:
     int maxBottlesDrunk(int numBottles, int x) {
