019548afe293c0f646a8200a3c3dd4ddd01f7dea
khushi <khushisangwan441@gmail.com>
2025-10-04 16:17:33 +0530

    refactor: enhance comments for clarity in maxArea function

--- Day_314_11_Container_With_Most_Water.cpp
+++ Day_314_11_Container_With_Most_Water.cpp
@@ -4,11 +4,15 @@ class Solution {
 public:
     int maxArea(vector<int>& h){
         int n = h.size();
-        int i = 0, j = n - 1;
+        int i = 0, j = n - 1;  // Two pointers at start and end
         int ans = 0;
+        
         while(i < j){
-            int x = min(h[i], h[j]);
-            ans = max(ans, x * (j - i));
+            // Calculate area with current pointers
+            int x = min(h[i], h[j]);  // Height is limited by shorter line
+            ans = max(ans, x * (j - i));  // Width is distance between pointers
+            
+            // Move pointer with smaller height inward
             if(h[i] > h[j]){
                 j--;
             }
