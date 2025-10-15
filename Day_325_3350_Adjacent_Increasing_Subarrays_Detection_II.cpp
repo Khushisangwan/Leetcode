fa29c4ef8a861fb08985d85dd9ac498ebdf9b00b
khushi <khushisangwan441@gmail.com>
2025-10-15 23:14:51 +0530

    refactor: enhance code readability with improved variable declarations and comments in maxIncreasingSubarrays function

--- Day_325_3350_Adjacent_Increasing_Subarrays_Detection_II.cpp
+++ Day_325_3350_Adjacent_Increasing_Subarrays_Detection_II.cpp
@@ -4,16 +4,23 @@ class Solution {
 public:
     int maxIncreasingSubarrays(vector<int>& nums) {
         int n = nums.size();
-        int up = 1, preUp = 0, res = 0;
+        int up = 1;        // Length of current increasing subarray
+        int preUp = 0;     // Length of previous increasing subarray
+        int res = 0;       // Maximum length found so far
+        
         for (int i = 1; i < n; i++) {
-            if (nums[i] > nums[i - 1]) up++;
-            else {
-                preUp = up;
-                up = 1;
+            if (nums[i] > nums[i - 1]) {
+                up++;  // Extend current increasing subarray
+            } else {
+                preUp = up;  // Store previous subarray length
+                up = 1;      // Reset current subarray length
             }
-            int half = up >> 1;
-            int m = min(preUp, up);
-            int candidate = max(half, m);
+            
+            // Try two scenarios:
+            int half = up >> 1;           // Split current subarray in half
+            int m = min(preUp, up);       // Use previous and current subarrays
+            int candidate = max(half, m); // Take the better option
+            
             if (candidate > res) res = candidate;
         }
         return res;
