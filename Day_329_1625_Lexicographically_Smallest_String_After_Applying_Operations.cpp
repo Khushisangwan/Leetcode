9edb5318822e5a01bf8707a1693246ae7a6fc341
khushi <khushisangwan441@gmail.com>
2025-10-19 22:48:48 +0530

    refactor: enhance comments for clarity in findLexSmallestString function

--- Day_329_1625_Lexicographically_Smallest_String_After_Applying_Operations.cpp
+++ Day_329_1625_Lexicographically_Smallest_String_After_Applying_Operations.cpp
@@ -3,16 +3,24 @@ using namespace std;
 class Solution {
 public:
     string findLexSmallestString(string s, int a, int b) {
+        // Set to keep track of visited strings to avoid cycles
         unordered_set<string> vis;
+        // Initialize the smallest string as the original string
         string smallest = s;
+        // BFS queue to explore all possible transformations
         queue<string> q;
         q.push(s);
         vis.insert(s);
 
+        // BFS to explore all reachable strings
         while (!q.empty()) {
-            string cur = q.front(); q.pop();
+            string cur = q.front(); 
+            q.pop();
+            
+            // Update smallest string if current is lexicographically smaller
             if (cur < smallest) smallest = cur;
 
+            // Operation 1: Add 'a' to all odd-indexed digits
             string added = cur;
             for (int i = 1; i < added.size(); i += 2)
                 added[i] = ((added[i] - '0' + a) % 10) + '0';
@@ -21,6 +29,7 @@ public:
                 q.push(added);
             }
 
+            // Operation 2: Rotate string to the left by 'b' positions
             string rotated = cur.substr(cur.size() - b) + cur.substr(0, cur.size() - b);
             if (!vis.count(rotated)) {
                 vis.insert(rotated);
