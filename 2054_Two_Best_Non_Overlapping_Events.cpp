1b194f5ef34f8bcf25bbdfe355e40d70ec9a470e
khushi <khushisangwan441@gmail.com>
2025-12-23 15:38:49 +0530

    refactor: improve code readability by enhancing comments and formatting in maxTwoEvents method

--- 2054_Two_Best_Non_Overlapping_Events.cpp
+++ 2054_Two_Best_Non_Overlapping_Events.cpp
@@ -2,22 +2,34 @@
 using namespace std;
 class Solution {
 public:
-    using info=tuple<int, bool, int>;
+    // Tuple to store: time, isEnd (0=start, 1=end), value
+    using info = tuple<int, bool, int>;
+    
     static int maxTwoEvents(vector<vector<int>>& events) {
-        const int n=events.size();
-        vector<info> time(n*2);
-        for(int i=0; i<n; i++){
-            int s=events[i][0], e=events[i][1], v=events[i][2];
-            time[2*i]={s, 0, v};
-            time[2*i+1]={e, 1, v};
+        const int n = events.size();
+        
+        // Create events array with start and end times
+        vector<info> time(n * 2);
+        for (int i = 0; i < n; i++) {
+            int s = events[i][0], e = events[i][1], v = events[i][2];
+            time[2 * i] = {s, 0, v};           // Start event
+            time[2 * i + 1] = {e, 1, v};       // End event
         }
+        
+        // Sort by time, then by event type (ends before starts)
         sort(time.begin(), time.end());
-        int ans=0, maxV=0, n2=n*2;
-        for(auto& [t, isEnd, v]: time){
-            if (isEnd) maxV=max(maxV, v);
-            else ans=max(ans, maxV+v);
+        
+        int ans = 0, maxV = 0, n2 = n * 2;
+        
+        // Process events in chronological order
+        for (auto& [t, isEnd, v] : time) {
+            if (isEnd)                          // If event ends, update max value seen
+                maxV = max(maxV, v);
+            else                                // If event starts, combine with best previous
+                ans = max(ans, maxV + v);
         }
-        return ans;    
+        
+        return ans;
     }
 };
 
