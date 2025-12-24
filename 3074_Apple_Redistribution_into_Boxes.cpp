ccbc80687e707955f2d4c1bee081aa123263bc9a
khushi <khushisangwan441@gmail.com>
2025-12-24 23:15:16 +0530

    refactor: enhance comments for clarity in minimumBoxes method of 3074_Apple_Redistribution_into_Boxes.cpp

--- 3074_Apple_Redistribution_into_Boxes.cpp
+++ 3074_Apple_Redistribution_into_Boxes.cpp
@@ -3,9 +3,14 @@ using namespace std;
 class Solution {
 public:
     int minimumBoxes(vector<int>& apple, vector<int>& cap) {
+        // Calculate total number of apples to distribute
         int sum = accumulate(apple.begin(), apple.end(), 0);
+        
+        // Frequency array to count boxes of each capacity
         uint8_t fq[51] = {};
         int high = 0, low = 51;
+        
+        // Count frequency of each capacity and find min/max
         for (auto& c : cap) {
             fq[c]++;
             high = max(high, c);
@@ -13,7 +18,9 @@ public:
         }
 
         int res = 0;
+        // Greedily use boxes with largest capacity first
         for (int i = high; i >= low && sum > 0; i--) {
+            // Use all boxes of capacity i until no apples remain
             while (fq[i]-- > 0 && sum > 0) {
                 sum -= i;
                 res++;
