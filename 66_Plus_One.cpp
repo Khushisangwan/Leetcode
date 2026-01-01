3b2420487b629344193d7f04ce8c27c742e89519
khushi <khushisangwan441@gmail.com>
2026-01-01 23:18:25 +0530

    refactor: enhance comments for clarity in plusOne method

--- 66_Plus_One.cpp
+++ 66_Plus_One.cpp
@@ -3,13 +3,17 @@ using namespace std;
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
+        // Iterate from the last digit to the first
         for (int i = digits.size() - 1; i >= 0; i--) {
+            // If digit is not 9, increment it and return
             if (digits[i] != 9) {
                 digits[i] += 1;
                 return digits;
             }
+            // Set 9 to 0 and continue to the next digit
             digits[i] = 0;
         }
+        // If all digits were 9, insert 1 at the beginning
         digits.insert(digits.begin(), 1);
         return digits;     
     }
