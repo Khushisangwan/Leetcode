9a0ea91e8e3b45d05c7b9a216e20063e335df8d6
khushi <khushisangwan441@gmail.com>
2026-01-15 19:19:57 +0530

    refactor: improve code readability with consistent formatting and additional comments in 2943_Maximize_Area_of_Square_Hole_in_Grid.cpp

--- 2943_Maximize_Area_of_Square_Hole_in_Grid.cpp
+++ 2943_Maximize_Area_of_Square_Hole_in_Grid.cpp
@@ -3,20 +3,29 @@ using namespace std;
 class Solution {
 public:
     int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
+        // Sort horizontal and vertical bars
         sort(hBars.begin(), hBars.end());
         sort(vBars.begin(), vBars.end());
 
+        // Lambda function to find the maximum consecutive span of bars
         auto maxSpan = [](vector<int>& bars) {
             int res = 1, streak = 1;
+            // Count consecutive bars (bars that differ by exactly 1)
             for (int i = 1; i < bars.size(); i++) {
-                if (bars[i] - bars[i - 1] == 1) streak++;
-                else streak = 1;
+                if (bars[i] - bars[i - 1] == 1) {
+                    streak++;
+                } else {
+                    streak = 1;
+                }
                 res = max(res, streak);
             }
+            // Add 1 to convert span to hole size
             return ++res;
         };
 
+        // The square side length is limited by the minimum span
         int s = min(maxSpan(hBars), maxSpan(vBars));
+        // Return the area of the square
         return s * s;
     }
 };
