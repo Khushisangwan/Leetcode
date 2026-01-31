afea1acdddaa158ca9ea49741ed10bb89b45d622
khushi <khushisangwan441@gmail.com>
2026-01-31 13:37:32 +0530

    refactor: improve code documentation with comments for clarity in nextGreatestLetter function

--- 744_Find_Smallest_Letter_Greater_Than_Target.cpp
+++ 744_Find_Smallest_Letter_Greater_Than_Target.cpp
@@ -3,20 +3,22 @@ using namespace std;
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        char res = letters[0];
-        bool flag = false;
+        char res = letters[0];  // Initialize result with first letter
+        bool flag = false;      // Flag to track if we've found first letter > target
 
         for (char ch : letters) {
             if (!flag) {
+                // First iteration: find the first letter greater than target
                 if (ch > target) {
                     res = ch;
-                    flag = !flag;
+                    flag = !flag;  // Set flag to true
                 }
             } else {
+                // Subsequent iterations: find the smallest letter > target
                 if (ch > target && ch < res) res = ch;
             }
         }
 
-        return res;
+        return res;  // Return the smallest letter greater than target
     }
 };
