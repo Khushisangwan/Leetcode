d5f2ef008e0650ec66a9ecbef1e5f7010dfe6ba1
khushi <khushisangwan441@gmail.com>
2026-02-08 19:27:52 +0530

    feat: implement balanced binary tree check with modified_height function

---
+++ 110_Balanced_Binary_Tree.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int modified_height(TreeNode* root){
+        if(root == NULL) return 0;
+
+        int left_height = modified_height(root->left);
+        if( left_height == -1) return -1;
+
+        int right_height = modified_height(root->right);
+        if( right_height == -1) return -1;
+
+        if(abs(left_height - right_height) > 1) return -1;
+        return 1 + max(left_height, right_height);
+    }
+
+
+    bool isBalanced(TreeNode* root){
+        return modified_height(root) != -1;
+    }
+};
